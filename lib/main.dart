2df4b5fce7c75bdc04a92bcd3e30ab16abcfb346
murilo.parra <murilo.parra@gmail.com>
2019-12-13 14:50:44 -0300

    Criação do meu widget MyBar e Bola para reutilização de código

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
       ),
       home: MyHomePage(title: 'Mega-sena'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
